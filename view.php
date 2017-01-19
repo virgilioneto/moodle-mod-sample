f4a1beccceae6c794ee164eb4e87daba98782445
Virgilio Neto <virgilio.missao.neto@gmail.com>
2017-01-19 00:41:46 -0200

    Include js_call_amd to load component1.js module

--- view.php
+++ view.php
@@ -54,4 +54,6 @@ $data->myvar = "Some data to share with the template";
 $template = new \mod_sample\output\view_page($data);
 echo $output->render($template);
 
+$PAGE->requires->js_call_amd('mod_sample/component1', 'init', array($cm->id));
+
 echo $output->footer();
